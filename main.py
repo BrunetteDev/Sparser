59462e7e3d3c0e392c3a2944176928d542658e3e
Randy Brunet Charlot <randy.brunet@estudiantes.fisica.uh.cu>
2024-05-16 01:04:26 -0400

    Clean Code Complete

--- main.py
+++ main.py
@@ -6,52 +6,28 @@ from metaclass import Program
 import numpy as np
 import argparse
 
-parser = argparse.ArgumentParser(description='Compressed Sensigna 1-D')
-parser.add_argument('--rowmtx', '-m', help='Number of Row of Compresion Matrix')
-parser.add_argument('--colmtx', '-n', help='Number of Columns of the Compresion Matrix')
-parser.add_argument('--compresionratio', '-cr', help='Compresion Ratio (0-1)')
-parser.add_argument('--base', '-b', help="The base of the data representation \n (0 - Fourier, 1 - Haar Wavelet)")
-parser.add_argument('--startcol', '-ic', help="Initial value of the silde of the column of the data for to process")
-parser.add_argument('--lastcol', '-lc', help="Last value of the silde of the column of the data for to process")
-parser.add_argument('--path', '-p', help="Path of the file")
+parser = argparse.ArgumentParser(description='Compressed Sensing 1-D \n [Example] python main.py -n 11968 -cr 0.5 -b 0 -mc 1 -ic 1 -lc 2 -p <path> -ps <path save>')
+parser.add_argument('--rowmtx', '-m', help='\nNumber of Row of Compresion Matrix')
+parser.add_argument('--colmtx', '-n', help='\nNumber of Columns of the Compresion Matrix', type=int)
+parser.add_argument('--compresionratio', '-cr', help='\nCompresion Ratio (0-1)\n', type=float)
+parser.add_argument('--multicol', '-mc', help="\nUse the multicol of the file (0-False 1-True)", type=int)
+parser.add_argument('--base', '-b', help="\nThe base of the data representation \n (0 - Fourier, 1 - Haar Wavelet, 2 - Discrete Cosine)", type=int)
+parser.add_argument('--startcol', '-ic', help="\nInitial value of the silde of the column of the data for to process", type=int)
+parser.add_argument('--lastcol', '-lc', help="\nLast value of the silde of the column of the data for to process", type=int)
+parser.add_argument('--path', '-p', help="\nPath of the file", type=str)
+parser.add_argument('--pathsave', '-ps', help="\n Path to save the result", type=str)
+
 
 args = parser.parse_args()
 
 def main():
-    #data = Data("/media/darketo/31fcec1d-d7c9-4d4f-9770-1e1d235bda6f/randy/Investigation/Sparsing/Projects/SparseData/Data/Randy_Data/Experimental/gar_001.xy")
-    bas = Basis()
     path = "/media/darketo/31fcec1d-d7c9-4d4f-9770-1e1d235bda6f/randy/Investigation/Sparsing/Projects/SparseData/Data/Randy_Data/Experimental/gar_001.xy"
-    #func = data.load_data("        ")
-    # func = data.load_data(" ")
-    # print(func)
-    # func['x'] = data.re_scale_axis(func, 'x')
-    # func['y'] = data.re_scale_axis(func, 'y')
-    #data.export_json(func, './1')
-    # y = func['y']
-    # x = func['y']
     path = "../Sparser/Files/formated.txt"
-    # x = x[0:(len(x)//6)]
-    # y = y[0:len(y)//6]
-    # cs = Compressed_Sensing(70, y)
-    # N = len(y)
-    # fr_mtx = bas.build_fft_mtx(N)
-    #haar = bas.build_haar_mtx(y_len, False)
-    #haar = bas.truncate_mtx(haar, y_len)
-    #plt.plot(func['x'], fr_mtx @ np.array(func['y']))
-    #plt.plot(func['x'], haar.T @ np.array(func['y']))
-    #print(haar.shape)
-    # print(len(func['y']))
-    #plt.show()
 
-    # res = cs.compress_signal(fr_mtx, N)
-    # print(res)
-    # plt.plot(x, res)
-    # plt.show()
-    # print(cs.reconst_sig(res, fr_mtx))
     
-    for i in range(0, 100, 10):
-        program = Program(11968 // 8,i / 100,0,True, 1, 2, path)
-        program.run()
+#    for i in range(0, 100, 10):
+    program = Program(args.colmtx // 12,args.compresionratio,args.base,args.multicol, args.startcol, args.lastcol, args.path, args.pathsave)
+    program.run()
 
 if __name__ == '__main__':
     main()
--- metaclass.py
+++ metaclass.py
@@ -7,11 +7,12 @@ import scipy.fftpack as spfft
 import matplotlib.pyplot as plt
 
 class Program(Basis, Compressed_Sensing, Data):
-    def __init__(self, N: int, CR: float,  base: int, multidata: bool, slc_init: int, slc_end: int, path: str) -> None:
+    def __init__(self, N: int, CR: float,  base: int, multidata: bool, slc_init: int, slc_end: int, path: str, path_save_json: str) -> None:
         self.__N = N
         self.__CR = CR
         self.__base = base
         self.__path = path 
+        self.__path_save_json = path_save_json
 
         self.__M = int(self.__CR * self.__N)
         self.__slc_init = slc_init
@@ -36,28 +37,19 @@ class Program(Basis, Compressed_Sensing, Data):
             inv_mtx = bas.inv_bas(mtx)
             mtx = bas.truncate_mtx(mtx, self.__N)
             inv_mtx = bas.truncate_mtx(inv_mtx, self.__N)
-
-            for i in mtx:
-                print(i)
-            print("SEP")
-            for i in inv_mtx:
-                print(i)
         elif self.__base == 2:
             mtx = bas.idct_mtx(self.__N)
             inv_mtx = spfft.idct(mtx, norm="ortho", axis=0)
+
         ri = cs.choice_sample(self.__N, self.__M)
 
 
         cp_ri = np.sort(ri)
-        #mtx = mtx[ri]
         for i in range(len(cp_ri)):
             mtx = np.delete(mtx, cp_ri[i], 0)
             cp_ri -= cp_ri[i]
 
-        print(f"{mtx.shape} \n\n\n\n\n\n\n\n\n")
-        # measurment = [np.array(np.zeros(len(func['y']))) for i in range(self.__M)]
         col = func['y']
-
 
         for i in range(len(col)):
             aux = []
@@ -67,20 +59,28 @@ class Program(Basis, Compressed_Sensing, Data):
             col[i] = np.array(aux)
 
         
-        #col[i] = [val for val in col[i] if counter not in ri]
         for i in range(len(col)):
             inv = cs.compress_signal(mtx, self.__N, col[i])
+            # aux = [list(i) for i in mtx]
+            # aux2 = [float(i).__round__(4) for i in inv ]
+
+            # data.export_json({'y': aux2}, f"{self.__path_save_json}_[{i}].json")
+            # data.export_json({
+            #     'N': self.__N,
+            #     "cr": self.__CR,
+            #     "base": aux,
+            #     "slc_init": self.__slc_init,
+            #     "slc_end":self.__slc_end
+            # }, f"{self.__path_save_json}_[{i}]_metadata.json" )
+
 
             #Problema Inverso
-            if self.__base == 2:
-                inv = spfft.idct(inv, norm='ortho', axis=0)
-            else:
-                inv = cs.compress_signal(mtx @ inv_mtx, self.__N, col[i])
+            # if self.__base == 2:
+            #     inv = spfft.idct(inv, norm='ortho', axis=0)
+            # else:
+            #     inv = cs.compress_signal(mtx @ inv_mtx, self.__N, col[i])
 
-            figures, ax = plt.subplots(2)
-            ax[0].plot(np.linspace(0,1, len(col[i])), col[i])
-            ax[1].plot(np.linspace(0,1, len(inv)), inv)
-            plt.show()
-        # print(mtx.shape)
-        # print(len(col[0]))
-        # print(len(ri))
+            # figures, ax = plt.subplots(2)
+            # ax[0].plot(np.linspace(0,1, len(col[i])), col[i])
+            # ax[1].plot(np.linspace(0,1, len(inv)), inv)
+            # plt.show()
